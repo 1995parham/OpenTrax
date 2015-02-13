e9dc792f30ff2fec7020c4bb243dff07477c259f
Parham Alvani <parham.alvani@gmial.com>
2015-02-13 19:49:47 +0330

    Improving GUI part #4.

--- src/main/java/home/parham/gui/ChooseTile.java
+++ src/main/java/home/parham/gui/ChooseTile.java
@@ -1,28 +1,21 @@
 package home.parham.gui;
 
-import home.parham.gui.Tile;
-
 import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.image.BufferedImage;
-
-import javax.swing.BoxLayout;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JDialog;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
+import java.util.List;
+import javax.swing.*;
 
 public class ChooseTile extends JDialog {
 
-	private java.util.List<Tile> possibleMoves;
 	private JPanel buttons;
 	private int chosenMove;
-	
-	public ChooseTile(JFrame owner, java.util.List<Tile> possibleMoves) {
-		super(owner, "Choose move", true);
-		this.possibleMoves = possibleMoves;
+
+	public ChooseTile(JFrame owner, List<Tile> possibleMoves){
+		super(owner, "Choose Move", true);
+		
+		this.setLocationRelativeTo(owner);
+		
 		buttons = new JPanel();
 		chosenMove = -1;
 		buttons.setLayout(new BoxLayout(buttons, BoxLayout.X_AXIS));
@@ -34,43 +27,40 @@ public class ChooseTile extends JDialog {
 			button = new JButton();
 			button.setMargin(new Insets(0, 0, 0, 0));
 			button.setBorder(null);
-			button.setSize(90,90);
+			button.setSize(90, 90);
 			button.setText("No moves available");
 			button.addActionListener(new ActionListener() {
-	            public void actionPerformed(ActionEvent e) {
-	            	ChooseTile.this.setVisible(false);
-	            }
-	        });
+				public void actionPerformed(ActionEvent e){
+					ChooseTile.this.dispose();
+				}
+			});
 			buttons.add(button);
 		}
 		this.add(buttons);
 		this.pack();
 	}
 
-	public int getChosenMove() {
+	public int getChosenMove(){
 		return chosenMove;
 	}
-	
-	private void addImageButton(Tile possibleMove, final int pos) {
+
+	private void addImageButton(Tile possibleMove, final int pos){
 		JButton button;
 		button = new JButton();
 		button.setMargin(new Insets(0, 0, 0, 0));
 		button.setBorder(null);
-		button.setSize(90,90);
+		button.setSize(90, 90);
 		button.setIcon(new ImageIcon(possibleMove.getImage()));
-		button.setName(""+pos);
+		button.setName("" + pos);
 		button.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-            	ChooseTile.this.setVisible(false);
-            	chosenMove = pos;
-            }
-        });
+			public void actionPerformed(ActionEvent e){
+				ChooseTile.this.setVisible(false);
+				chosenMove = pos;
+			}
+		});
 		buttons.add(button);
 	}
-	
-	/**
-	 * 
-	 */
+
 	private static final long serialVersionUID = 8281287412217260698L;
 
 }
