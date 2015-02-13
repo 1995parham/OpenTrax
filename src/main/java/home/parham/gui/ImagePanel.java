644aad54d97f7d35da6705a891362ec059aad1df
Parham Alvani <parham.alvani@gmial.com>
2015-02-13 13:54:26 +0330

    Improving GUI part #1.

--- src/main/java/home/parham/gui/ImagePanel.java
+++ src/main/java/home/parham/gui/ImagePanel.java
@@ -1,31 +1,25 @@
 package home.parham.gui;
 
-import javax.swing.BorderFactory;
-import javax.swing.JDialog;
-import javax.swing.JPanel;
-import java.awt.image.*;
+import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
 
 public class ImagePanel extends JPanel {
 	private BufferedImage image;
 	private GnuTraxGui gnuTrax;
-	private int x,y;
-	
-	ImagePanel(BufferedImage image, GnuTraxGui gnuTrax, int x, int y) {
-		setImage(image);
+	private int x, y;
+
+	ImagePanel(BufferedImage image, GnuTraxGui gnuTrax, int x, int y){
+		this.setImage(image);
 		this.gnuTrax = gnuTrax;
 		this.addMouseListener(new ImageClickHandler());
+		this.setSize(new Dimension(80, 80));
 		this.setBorder(BorderFactory.createLineBorder(Color.black));
-		this.setPreferredSize(new Dimension(80, 80));
 		this.x = x;
 		this.y = y;
 	}
 
-	ImagePanel() {
-		image = null;
-	}
-
-	public void showMovesDialog() {
+	public void showMovesDialog(){
 		java.util.List<Tile> possibleMoves = gnuTrax.getPossibleTilesForPosition(this.x, this.y);
 		ChooseTile ct = new ChooseTile(this.gnuTrax, possibleMoves);
 		ct.setVisible(true);
@@ -33,16 +27,16 @@ public class ImagePanel extends JPanel {
 			this.gnuTrax.setMove(this.x, this.y, possibleMoves.get(ct.getChosenMove()));
 		}
 	}
-	
-	public void setImage(BufferedImage image) {
+
+	public void setImage(BufferedImage image){
 		this.image = image;
 	}
-	
-	public BufferedImage getImage() {
+
+	public BufferedImage getImage(){
 		return this.image;
 	}
 
-	protected void paintComponent(Graphics g) {
+	protected void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.drawImage(image, 0, 0, this);
 	}
